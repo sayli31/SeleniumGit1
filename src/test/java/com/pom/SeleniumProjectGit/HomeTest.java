b635018aad0361dc9a0ab203f8160a24e3f8b666
sayali <s@gmail.com>
2021-08-04 12:08:13 +0530

    Home file

--- src/test/java/com/pom/SeleniumProjectGit/HomeTest.java
+++ src/test/java/com/pom/SeleniumProjectGit/HomeTest.java
@@ -4,5 +4,8 @@ public class HomeTest {
 	
 	String name;
 	int j =100;
+	public static void main(String[] args) {
+		
+	}
 
 }
--- src/test/java/com/pom/SeleniumProjectGit/LoginTest.java
+++ src/test/java/com/pom/SeleniumProjectGit/LoginTest.java
@@ -5,6 +5,7 @@ public class LoginTest {
 	int i =10;
 	String name;
 	int j;
+	int k;
 	
 	public static void main(String[] args) {
 		
